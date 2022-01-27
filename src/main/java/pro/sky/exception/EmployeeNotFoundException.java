12f1a558a76e662e7c05d10ff631bdd1a0fd5661
Anatoliy <t7313@ya.ru>
2022-01-27 23:18:41 +0300

    Hw 2.4 is done.

---
+++ src/main/java/pro/sky/exception/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package pro.sky.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException() {
+        super("Employee not found");
+    }
+
+}
